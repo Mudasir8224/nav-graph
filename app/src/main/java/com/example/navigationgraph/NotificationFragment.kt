1a3f3b628f5ba5ea18ae303c78e86530ce8fda71
DESKTOP-0D7QI3N\Power <mohammad.mudasir@hyfatech.com>
2022-06-14 19:43:05 +0500

    add viewmodel

--- app/src/main/java/com/example/navigationgraph/NotificationFragment.kt
+++ app/src/main/java/com/example/navigationgraph/NotificationFragment.kt
@@ -1,19 +1,40 @@
 package com.example.navigationgraph
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.navigationgraph.databinding.FragmentNotificationBinding
 
 class NotificationFragment : Fragment() {
-
+    private var _binding: FragmentNotificationBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+
+        val notificationViewModel = ViewModelProvider(this).get(NotificationViewModel::class.java)
+
+        _binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotification
+
+        notificationViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        return root
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
---
+++ app/src/main/java/com/example/navigationgraph/NotificationViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.navigationgraph
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notification Fragment"
+    }
+    val text: LiveData<String> = _text
+}
